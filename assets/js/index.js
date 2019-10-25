568f5c1abdd26867957e147538d3f50a9d78bf20
Emmanuel <Emmanuelsumeh@gmail.com>
2019-10-25 04:58:47 -0700

    Updates

--- assets/js/index.js
+++ assets/js/index.js
@@ -136,30 +136,7 @@ window.addEventListener('load', async () => {
     console.log(games.name)
     console.log(games.price)
 
-    $(document).ready(function() {
-      var canDownload = true;
-      $('.buy').on('click', function() {
-        if(canDownload === true) {
-          $(this).addClass('loading').find('span, small').hide();
-          setTimeout(function() {
-            $('.loading').addClass('processing');
-            canDownload = false;
-            setTimeout(function() {
-              $('.buy').removeClass('processing');
-              setTimeout(function() {
-                $('.buy').removeClass('loading').addClass('success').find('.fa-check').fadeIn(100);
-                setTimeout(function() {
-                  $('.download').addClass('active').children().fadeIn(100, function() {
-                    $('meta[name="theme-color"]').attr('content','#21d49a');
-                    $('.buy').css('background-color','#21d49a');
-                  });
-                }, 1000);
-              }, 400);
-            }, 2800);
-          }, 300);
-        }
-      });
-    });
+   
 
     GameArray.push({
         imageUrl : games.images,
