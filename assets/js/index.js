8751afbe4e4271e5c6d32abe541b6dc6e18711e9
Emmanuel <Emmanuelsumeh@gmail.com>
2019-10-26 01:08:27 -0700

    updates

--- assets/js/index.js
+++ assets/js/index.js
@@ -73,9 +73,9 @@ contract Gamify =
     
     Chain.spend(_seller, Call.value)
  
-    `; 
-    
-    
+    `;
+
+
 
 
 const contractAddress = 'ct_oBWZxezUbHRWBBeLKB2LR59ms8rLu2NGhX7mCDWqA94SW8DE1';
@@ -88,38 +88,49 @@ var gameLength = 0;
 
 
 
-function renderProduct()
-{
-    GameArray = GameArray.sort(function(a,b){return b.Price - a.Price})
-    var template = $('#template').html();
-    
-    Mustache.parse(template);
-    var rendered = Mustache.render(template, {GameArray});
+function renderProduct() {
+  GameArray = GameArray.sort(function (a, b) {
+    return b.Price - a.Price
+  })
+  var template = $('#template').html();
 
-    
-  
+  Mustache.parse(template);
+  var rendered = Mustache.render(template, {
+    GameArray
+  });
 
-    $('#body').html(rendered);
-    console.log("for loop reached")
+
+
+
+  $('#body').html(rendered);
+  console.log("for loop reached")
 }
 //Create a asynchronous read call for our smart contract
 async function callStatic(func, args) {
   //Create a new contract instance that we can interact with
-  const contract = await client.getContractInstance(contractSource, {contractAddress});
+  const contract = await client.getContractInstance(contractSource, {
+    contractAddress
+  });
   //Make a call to get data of smart contract func, with specefied arguments
   console.log("Contract : ", contract)
-  const calledGet = await contract.call(func, args, {callStatic: true}).catch(e => console.error(e));
+  const calledGet = await contract.call(func, args, {
+    callStatic: true
+  }).catch(e => console.error(e));
   //Make another call to decode the data received in first call
-  console.log("Called get found: ",  calledGet)
+  console.log("Called get found: ", calledGet)
   const decodedGet = await calledGet.decode().catch(e => console.error(e));
   console.log("catching errors : ", decodedGet)
   return decodedGet;
 }
 
 async function contractCall(func, args, value) {
-  const contract = await client.getContractInstance(contractSource, {contractAddress});
+  const contract = await client.getContractInstance(contractSource, {
+    contractAddress
+  });
   //Make a call to write smart contract func, with aeon value input
-  const calledSet = await contract.call(func, args, {amount:value}).catch(e => console.error(e));
+  const calledSet = await contract.call(func, args, {
+    amount: value
+  }).catch(e => console.error(e));
 
   return calledSet;
 }
@@ -129,123 +140,137 @@ window.addEventListener('load', async () => {
 
   client = await Ae.Aepp()
 
-  gameLength = await callStatic('getGameLength', []); 
+  gameLength = await callStatic('getGameLength', []);
 
-  for(let i = 1; i<= gameLength ; i++ ){
-    const games =  await callStatic('get_game_by_index', [i]);
-    
-    console.log("for loop reached", "pushing to array")
-    console.log(games.images)
-    console.log(games.name)
-    console.log(games.price)
 
-   
 
-    GameArray.push({
-        id : i,
-        imageUrl : games.images,
-        name : games.name, 
-        price : games.price,
-        purchased : games.purchased
-        
+  for (let i = 1; i <= gameLength; i++) {
+    const games = await callStatic('get_game_by_index', [i]);
 
-     
+    if (games.purchase) {
+      SoldArray({
+        id: i,
+        imageUrl: games.images,
+        name: games.name,
+        price: games.price,
+        purchased: games.purchased
+      })
+
+    } else {
+      console.log("for loop reached", "pushing to array")
+      console.log(games.images)
+      console.log(games.name)
+      console.log(games.price)
+
+
+
+      GameArray.push({
+        id: i,
+        imageUrl: games.images,
+        name: games.name,
+        price: games.price,
+        purchased: games.purchased
+
+      })
+    }
+
+    renderProduct();
+    $("#loadings").hide();
+  }
+});
+
+
+
+$('#regButton').click(async function () {
+  $("#loadings").show();
+
+  var name = ($('#name').val()),
+
+    url = ($('#imageUrl').val()),
+
+    price = ($('#price').val());
+
+  description = ($('#description').val());
+  prices = parseInt(price, 10)
+  await contractCall('addGame', [name, prices, url, description], prices)
+
+  console.log(url)
+  console.log(name)
+  console.log(prices)
+  console.log(typeof (prices))
+
+
+
+  GameArray.push({
+    id: GameArray.id + 1,
+    name: name,
+    url: url,
+    price: prices,
+    description: description
+
+
+
   })
-}
   renderProduct();
   $("#loadings").hide();
 });
 
-
-
-$('#regButton').click(async function(){
+$("#body").click(".btn", async function (event) {
   $("#loadings").show();
+  console.log("Button has been clicked")
 
-    var name =($('#name').val()),
-    
-    url = ($('#imageUrl').val()),
-   
-    price = ($('#price').val());
+  // await contractCall('buyGame', [], prices)s
 
-    description = ($('#description').val());
-    prices = parseInt(price,10)
-    await contractCall('addGame', [name,prices,url,description], prices)
-   
-    console.log(url)
-    console.log(name)
-    console.log(prices)
-    console.log(typeof(prices))
-   
+  const dataIndex = event.target.id
+  const gamePrice = GameArray[dataIndex].price
+  console.log(gamePrice)
+  const gameid = GameArray[dataIndex].id
+  console.log("Price of product", gamePrice)
 
-    
-    GameArray.push({
-        id : GameArray.id + 1,
-        name : name,
-        url : url,
-        price : prices,
-        description : description
 
-        
-        
+  if (GameArray[dataIndex].purchased = false) {
+    await contractCall('buyGame', [dataIndex], parseInt(gamePrice, 10))
+    const game = await contractCall('get_game_by_index', [dataIndex])
+
+    SoldArray.push({
+      soldurl: game.images,
+      soldName: game.name,
+      soldPrice: game.price,
+      soldDescription: game.description,
+      newOwner: game.owner
+
     })
-    renderProduct();
-    $("#loadings").hide();
-});
 
-$("#body").click(".btn-2", async function(event){
-  $("#loadings").show();
-    console.log("Button has been clicked")
+  } else {
+    const messageId = document.getElementById(`${gameid}`)
+    console.log(messageId)
 
-    // await contractCall('buyGame', [], prices)s
-   
-    const dataIndex = event.target.id
-    const gamePrice = GameArray[dataIndex -1].price
-    console.log(gamePrice)
-    const gameid =  GameArray[dataIndex].id - 1
-    console.log("Price of product",gamePrice)
+    messageId.innerHTML = "Already Purchased";
 
-    
-    if(GameArray[dataIndex].purchased = false){
-      await contractCall('buyGame', [dataIndex],parseInt(gamePrice, 10))
-      const game = await contractCall('get_game_by_index', [dataIndex])
-      SoldArray.push({
-        soldurl : game.images,
-        soldName : game.name, 
-        soldPrice : game.price,
-        soldDescription : game.description
-        
-      })
 
-    }else{
-      const messageId = document.getElementById(`${gameid}`)
-      console.log(messageId)
-      
-      messageId.innerHTML = "Already Purchased";
-      
-     
-    }
+  }
   // sold = purchased_game.purchased 
-    // GameArray.push({
-    //   purchased : p
-      
+  // GameArray.push({
+  //   purchased : p
 
-    // })
-  
+
+  // })
+
   // const foundIndex = productListArr.findIndex(product => product.id === dataIndex)
   // const value = $(".buyBtn")[foundIndex] ;
 
-    console.log("-----------------")
-    console.log("Data Index:", dataIndex)
-    console.log("--------------------------")
-  
-    console.log("Just Clicked The Buy Button")
+  console.log("-----------------")
+  console.log("Data Index:", dataIndex)
+  console.log("--------------------------")
 
-    
-
-    
+  console.log("Just Clicked The Buy Button")
 
 
-    
-    renderProduct();
-    $("#loadings").hide();
+
+
+
+
+
+  renderProduct();
+  $("#loadings").hide();
 });
