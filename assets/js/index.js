4a8d309d4f2b03777106184e85ad225510438f39
Emmanuel <Emmanuelsumeh@gmail.com>
2019-11-17 10:25:51 +0100

    ipfs

--- assets/js/index.js
+++ assets/js/index.js
@@ -177,7 +177,44 @@ window.addEventListener('load', async () => {
 
 
 
-// $('#regButton').click(async function () {
+
+
+
+$('#regButton').click(async function () {
+
+  const ipfs = ipfsApi({
+    host: 'localhost',
+    port: '5001',
+    protocol: 'http'
+  })
+  
+  async function uploadFile(file) {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader()
+      reader.onloadend = () => {
+        const buffer = Buffer.from(reader.result)
+        ipfs.add(buffer)
+        .then(files => {
+          resolve(files)
+        })
+        .catch(error => reject(error))
+      }
+      reader.readAsArrayBuffer(file)
+    })
+  }
+  
+  async function onImageChange(event) {
+    const file = event.target.files[0]
+    const files = await uploadFile(file)
+    const multihash = files[0].hash
+    console.log(multihash)
+  }
+  
+  const file = document.querySelector('#file')
+  
+  file.addEventListener('change', onImageChange)
+
+})
 //   $("#loadings").show();
 
 //   var name = ($('#name').val()),
